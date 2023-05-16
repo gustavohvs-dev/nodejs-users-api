001dcdb8ab0c5637cf3dd67b7ed05e6e7bb01fea
Gustavo Soares <gustavohvs.dev@gmail.com>
2023-05-16 17:30:56 -0300

    first test

--- dist/config/config.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWTSecret = void 0;
-exports.JWTSecret = "SWLm;UYX'K-yna}+_Z";
---
+++ src/test/testando.test.ts
@@ -0,0 +1,10 @@
+let supertest = require("supertest")
+let request = supertest("www.google.com")
+
+describe("Grupo de teste 1", () => {
+
+    test("Descrição de teste exemplo 1", () => {
+        request.get("/").then(res => expect(res.statusCode).toEqual(200));
+    })
+
+})
